5a27947a33745d3457c36fd67ff2edb3ca6486d9
Sanket Mane <sanket.mane@gmail.com>
2026-08-03 01:00:24 +0530

    added EmailService

---
+++ EmailService/src/main/java/com/example/emailservice/consumers/EmailConsumer.java
@@ -0,0 +1,53 @@
+package com.example.emailservice.consumers;
+
+import com.example.emailservice.dtos.EmailDto;
+import com.example.emailservice.util.EmailUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import java.util.Properties;
+
+@Component
+public class EmailConsumer {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    // @KafkaListener annotation helps to declare a method
+    // that will be called when a topic arrives at the consumer side.
+    // groupId is used when there are multiple instances like ec2
+    // which can receive same topic at a given time and can generate multiple emails.
+    // To avoid that, groupId selects only 1 instance to do the actual work.
+    @KafkaListener(topics = "signup", groupId = "emailService")
+    public void sendEmail(String message) {
+        try {
+            EmailDto emailDto = objectMapper.readValue(message, EmailDto.class);
+
+            Properties props = new Properties();
+            props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+            props.put("mail.smtp.port", "587"); //TLS Port
+            props.put("mail.smtp.auth", "true"); //enable authentication
+            props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+
+            //create Authenticator object to pass in Session.getInstance argument
+            Authenticator auth = new Authenticator() {
+                //override the getPasswordAuthentication method
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(emailDto.getFrom(), ""); // add pass separately
+                }
+            };
+            Session session = Session.getInstance(props, auth); // establish email session
+
+            EmailUtil.sendEmail(session, emailDto.getTo(), emailDto.getSubject(), emailDto.getBody());
+
+        }catch (JsonProcessingException exception) {
+            throw new RuntimeException(exception.getMessage());
+        }
+    }
+}
---
+++ EmailService/src/main/java/com/example/emailservice/dtos/EmailDto.java
@@ -0,0 +1,13 @@
+package com.example.emailservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailDto {
+    String to;
+    String from;
+    String subject;
+    String body;
+}
