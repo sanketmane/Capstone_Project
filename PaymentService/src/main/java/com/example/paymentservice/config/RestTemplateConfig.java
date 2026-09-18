2d8600b21be8950d74483e8fe81d1aea68753381
Sanket Mane <sanket.mane@gmail.com>
2026-09-19 00:34:50 +0530

    Added Eureka service for ServiceDiscovery

--- PaymentService/src/main/java/com/example/paymentservice/config/RestTemplateConfig.java
+++ PaymentService/src/main/java/com/example/paymentservice/config/RestTemplateConfig.java
@@ -1,6 +1,7 @@
 package com.example.paymentservice.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
+    @LoadBalanced
     RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         // HttpComponents factory ensures PATCH requests work (JDK's HttpURLConnection doesn't support PATCH)
         return restTemplateBuilder.requestFactory(() -> new HttpComponentsClientHttpRequestFactory()).build();
